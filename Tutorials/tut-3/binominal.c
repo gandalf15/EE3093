a9a0b18eab817154b74755dd0429b48ff9da1a38
Marcel Zak <marcel@marcel-laptop.lan>
2017-10-02 23:35:44 +0100

    tutorial 3 finished and deleted compiled files .o

--- Tutorials/tut-3/binominal.c
+++ Tutorials/tut-3/binominal.c
@@ -11,12 +11,18 @@ int main ()
 	printf("\nBinominal expansion (a + b)n:");
 	printf("\nWrite n = ");
 	scanf("%d", &n);
+	printf("\n(a+b)^%d = ", n);
 	if (n == 0){
-		printf("\n1\n");
+		printf("\n1\n\n");
 		return 0;
 	}
 	for(int r = 0; r <= n; ++r){
-		printf("(%d * a^%d * b^%d) + ", fun(n,r), n-r, r);
+		if (r == n){
+			printf("(%da^%d * b^%d)\n\n ", fun(n,r), n-r, r);
+		}
+		else{
+			printf("(%da^%d * b^%d) + ", fun(n,r), n-r, r);
+		}
 	}
 
 	return 0;
